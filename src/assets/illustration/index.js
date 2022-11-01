9028157ae3534519e4e2402ed90ddc7d222cd6d5
cendekialnazalia <cendekialnazalia@gmail.com>
2022-11-01 20:54:45 +0700

    user profile

--- src/assets/illustration/index.js
+++ src/assets/illustration/index.js
@@ -1,5 +1,15 @@
 import ILLogo from './logo.svg';
 import ILGetStarted from './get-started.png';
 import ILNullPhoto from './null-photos.png';
+import ILCatUmum from './cat-dok-umum.svg';
+import ILCatPsikiater from './cat-dok-psikiater.svg';
+import ILCatObat from './cat-dok-obat.svg';
 
-export {ILLogo, ILGetStarted, ILNullPhoto};
+export {
+  ILLogo,
+  ILGetStarted,
+  ILNullPhoto,
+  ILCatObat,
+  ILCatPsikiater,
+  ILCatUmum,
+};

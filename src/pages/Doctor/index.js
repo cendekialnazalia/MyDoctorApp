e5943c5a9a50f5b3e7a60eaa9e163caba7202f77
cendekialnazalia <cendekialnazalia@gmail.com>
2022-11-01 19:41:47 +0700

    Doctor Base Component

--- src/pages/Doctor/index.js
+++ src/pages/Doctor/index.js
@@ -1,10 +1,29 @@
-import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {
+  DoctorCategory,
+  HomeProfile,
+  NewsItem,
+  RatedDoctor,
+} from '../../components';
 
 const Doctor = () => {
   return (
     <View>
-      <Text>Doctor Page</Text>
+      <HomeProfile />
+      <Text>Mau konsultasi dengan siapa hari ini?</Text>
+      <DoctorCategory />
+      <DoctorCategory />
+      <DoctorCategory />
+      <DoctorCategory />
+      <Text>Top Rated Doctors</Text>
+      <RatedDoctor />
+      <RatedDoctor />
+      <RatedDoctor />
+      <Text>Good News</Text>
+      <NewsItem />
+      <NewsItem />
+      <NewsItem />
     </View>
   );
 };

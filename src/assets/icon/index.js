f6f2685233f89d08e67b327062dfbbd0b723e644
cendekialnazalia <cendekialnazalia@gmail.com>
2022-11-02 15:03:40 +0700

    Slicing Rated Doctor

--- src/assets/icon/index.js
+++ src/assets/icon/index.js
@@ -7,6 +7,7 @@ import IconHospitalActive from './ic-hospital-active.svg';
 import IconHospitals from './ic-hospital.svg';
 import IconMessageActive from './ic-messages-active.svg';
 import IconMessage from './ic-messages.svg';
+import IconStar from './ic-star.svg';
 
 export {
   IconBackDark,
@@ -18,4 +19,5 @@ export {
   IconHospitals,
   IconMessageActive,
   IconMessage,
+  IconStar,
 };

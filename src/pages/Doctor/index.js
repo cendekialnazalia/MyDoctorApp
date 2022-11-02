bd32c6120e7dccaeb3784e0ecc0738efe874ee85
cendekialnazalia <cendekialnazalia@gmail.com>
2022-11-02 14:44:05 +0700

    slicing Category Doctor

--- src/pages/Doctor/index.js
+++ src/pages/Doctor/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {ScrollView} from 'react-native-gesture-handler';
 import {
   DoctorCategory,
+  Gap,
   HomeProfile,
   NewsItem,
   RatedDoctor,
@@ -11,20 +13,32 @@ import {colors, fonts} from '../../utils';
 const Doctor = () => {
   return (
     <View style={styles.page}>
-      <HomeProfile />
-      <Text style={styles.welcome}>Mau konsultasi dengan siapa hari ini?</Text>
-      <DoctorCategory />
-      <DoctorCategory />
-      <DoctorCategory />
-      <DoctorCategory />
-      <Text>Top Rated Doctors</Text>
-      <RatedDoctor />
-      <RatedDoctor />
-      <RatedDoctor />
-      <Text>Good News</Text>
-      <NewsItem />
-      <NewsItem />
-      <NewsItem />
+      <View style={styles.content}>
+        <HomeProfile />
+        <Text style={styles.welcome}>
+          Mau konsultasi dengan siapa hari ini?
+        </Text>
+        <View style={styles.wrapperScroll}>
+          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            <View style={styles.category}>
+              <Gap width={16} />
+              <DoctorCategory />
+              <DoctorCategory />
+              <DoctorCategory />
+              <DoctorCategory />
+              <Gap width={6} />
+            </View>
+          </ScrollView>
+        </View>
+        <Text style={styles.sectionLab}>Top Rated Doctors</Text>
+        <RatedDoctor />
+        <RatedDoctor />
+        <RatedDoctor />
+        <Text style={styles.sectionLab}>Good News</Text>
+        <NewsItem />
+        <NewsItem />
+        <NewsItem />
+      </View>
     </View>
   );
 };
@@ -32,7 +46,18 @@ const Doctor = () => {
 export default Doctor;
 
 const styles = StyleSheet.create({
-  page: {paddingVertical: 30, paddingHorizontal: 16},
+  page: {
+    backgroundColor: colors.secondary,
+    flex: 1,
+  },
+  content: {
+    backgroundColor: colors.white,
+    flex: 1,
+    paddingVertical: 30,
+    paddingHorizontal: 16,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+  },
   welcome: {
     fontSize: 20,
     fontFamily: fonts.primary[600],
@@ -40,5 +65,14 @@ const styles = StyleSheet.create({
     marginTop: 30,
     marginBottom: 16,
     maxWidth: 209,
+  },
+  category: {flexDirection: 'row'},
+  wrapperScroll: {marginHorizontal: -16},
+  sectionLab: {
+    fontSize: 16,
+    fontFamily: fonts.primary[600],
+    color: colors.text.primary,
+    marginTop: 30,
+    marginBottom: 16,
   },
 });

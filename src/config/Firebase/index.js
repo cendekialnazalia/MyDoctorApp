2cd9c871d24c4338694d603af9285b057a11aa27
cendekialnazalia <cendekialnazalia@gmail.com>
2022-11-11 12:50:51 +0700

    Setup Firebase di aplikasi React Native

---
+++ src/config/Firebase/index.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+firebase.intializeApp({
+  apiKey: 'AIzaSyC0xNj-1hUBRpic4Qs1w8iKnS9JBuDrsOM',
+  authDomain: 'my-doctor-01-daa74.firebaseapp.com',
+  projectId: 'my-doctor-01-daa74',
+  storageBucket: 'my-doctor-01-daa74.appspot.com',
+  messagingSenderId: '903008849438',
+  appId: '1:903008849438:web:2ff5452bf49e1362b6e80f',
+});
+
+const Firebase = firebase;
+
+export default Firebase;

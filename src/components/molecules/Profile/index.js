7c38c3417737546f56f77b3ed84857582f99be53
cendekialnazalia <cendekialnazalia@gmail.com>
2023-01-11 16:54:28 +0700

    edit photo in edit profile use local storage

--- src/components/molecules/Profile/index.js
+++ src/components/molecules/Profile/index.js
@@ -2,14 +2,23 @@ import {Image, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {DummyUser, IconRemovePhoto} from '../../../assets';
 import {colors, fonts} from '../../../utils';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
-const Profile = ({name, desc, isRemove, photo}) => {
+const Profile = ({name, desc, isRemove, photo, onPress}) => {
   return (
     <View style={styles.container}>
-      <View style={styles.borderProfile}>
-        <Image source={photo} style={styles.avatar} />
-        {isRemove && <IconRemovePhoto style={styles.removePhoto} />}
-      </View>
+      {!isRemove && (
+        <View style={styles.borderProfile}>
+          <Image source={photo} style={styles.avatar} />
+          {isRemove && <IconRemovePhoto style={styles.removePhoto} />}
+        </View>
+      )}
+      {isRemove && (
+        <TouchableOpacity style={styles.borderProfile} onPress={onPress}>
+          <Image source={photo} style={styles.avatar} />
+          {isRemove && <IconRemovePhoto style={styles.removePhoto} />}
+        </TouchableOpacity>
+      )}
       {name && (
         <View>
           <Text style={styles.name}>{name}</Text>
--- src/pages/UpdateProfile/index.js
+++ src/pages/UpdateProfile/index.js
@@ -1,10 +1,12 @@
 import {StyleSheet, View} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import {Profile, Header, Input, Button, Gap} from '../../components';
-import {colors, getData} from '../../utils';
+import {colors, getData, storeData} from '../../utils';
 import {ScrollView} from 'react-native-gesture-handler';
 import {Firebase} from '../../config';
 import {showMessage} from 'react-native-flash-message';
+import {launchImageLibrary} from 'react-native-image-picker';
+import {ILNullPhoto} from '../../assets';
 
 const UpdateProfile = ({navigation}) => {
   const [profile, setProfile] = useState({
@@ -13,11 +15,13 @@ const UpdateProfile = ({navigation}) => {
     email: '',
   });
   const [password, setPassword] = useState('');
+  const [photoForDB, setPhotoForDB] = useState('');
+  const [photo, setPhoto] = useState(ILNullPhoto);
 
   useEffect(() => {
     getData('user').then(res => {
       const data = res;
-      data.photo = {uri: res.photo};
+      setPhoto({uri: res.photo});
       setProfile(data);
     });
   });
@@ -25,12 +29,14 @@ const UpdateProfile = ({navigation}) => {
   const update = () => {
     console.log('profile', profile);
     const data = profile;
-    data.photo = profile.photo.uri;
+    data.photo = photoForDB;
+
     Firebase.database()
       .ref(`users/${profile.uid}/`)
-      .update(profile)
+      .update(data)
       .then(() => {
         console.log('success : ');
+        storeData('user', data);
       })
       .catch(err => {
         showMessage({
@@ -49,12 +55,34 @@ const UpdateProfile = ({navigation}) => {
     });
   };
 
+  const getImage = () => {
+    launchImageLibrary(
+      {quality: 0.5, maxWidth: 200, maxHeight: 200},
+      response => {
+        console.log('response: ', response);
+        if (response.didCancel || response.error) {
+          showMessage({
+            message: 'oops, sepertinya anda tidak memilih fotonya?',
+            type: 'default',
+            backgroundColor: colors.error,
+            color: colors.white,
+          });
+        } else {
+          console.log('response getImage: ', response);
+          const source = {uri: response.assets[0].uri};
+
+          setPhotoForDB(`data:${response.type};base64, ${response.data}`);
+          setPhoto(source);
+        }
+      },
+    );
+  };
   return (
     <View style={styles.page}>
       <Header title="Edit Profile" onPress={() => navigation.goBack()} />
       <ScrollView showsVerticalScrollIndicator={false}>
         <View style={styles.content}>
-          <Profile isRemove photo={profile.photo} />
+          <Profile isRemove photo={photo} onPress={getImage} />
           <Gap height={26} />
           <Input
             label="Full Name"
